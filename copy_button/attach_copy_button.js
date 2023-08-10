df71ac7b7a81511580ed2a9f44c75e35a70087ac
renzhamin <renzhamin@gmail.com>
2023-08-11 02:32:41 +0600

    test folder

---
+++ copy_button/attach_copy_button.js
@@ -0,0 +1,31 @@
+// @grant           GM_getResourceText
+// @grant           GM_addStyle
+// @resource        styles_copy_button styles_copy_button.css
+// @require         attach_copy_button.js
+
+function wrap(elem, wrapper) {
+    elem.parentNode.insertBefore(wrapper, elem)
+    wrapper.appendChild(elem)
+}
+
+function attach_copy_btn() {
+    GM_addStyle(GM_getResourceText("styles_copy_button"))
+
+    const codes = document.querySelectorAll("pre > code")
+    codes.forEach((code) => {
+        const pre = code.parentElement
+        const btn = document.createElement("button")
+        btn.textContent = "copy"
+        btn.classList.add("copy-btn")
+        btn.addEventListener("click", () =>
+            navigator.clipboard.writeText(code.textContent).then(() => {
+                btn.textContent = "copied"
+                setTimeout(() => (btn.textContent = "copy"), 2500)
+            })
+        )
+        const div = document.createElement("div")
+        div.classList.add("pre-wrapper")
+        div.appendChild(btn)
+        wrap(pre, div)
+    })
+}
