2e0494c264ca41555d0634ca683b58fbfbcd808c
renzhamin <renzhamin@gmail.com>
2023-08-11 01:39:23 +0600

    testing

---
+++ add_toc.js
@@ -0,0 +1,88 @@
+// ==UserScript==
+// @resource   styles_toc styles_toc.css
+// @grant      GM_getResourceText
+// @grant      GM_addStyle
+// ==/UserScript==
+
+function add_toc(toc_toggle_key) {
+    GM_addStyle(GM_getResourceText(styles_toc))
+
+    add_toc_toggle()
+    document.getElementById("toc").innerHTML += generate_toc()
+
+    if (toc_toggle_key) {
+        document.addEventListener("keydown", (event) => {
+            if (event.key === toc_toggle_key) {
+                show_toc()
+            }
+        })
+    }
+}
+
+function show_toc() {
+    document.documentElement.style.overflow = "hidden"
+    document.getElementById("toc_dialog").showModal()
+}
+
+function add_toc_toggle() {
+    const dialog = document.createElement("dialog")
+    dialog.setAttribute("id", "toc_dialog")
+    const toc_div = document.createElement("div")
+    toc_div.setAttribute("id", "toc")
+    toc_div.innerHTML = "<h1>Table of Contents</h2>"
+    dialog.appendChild(toc_div)
+
+    const show_toc_btn = document.createElement("button")
+    show_toc_btn.setAttribute("id", "show_toc_btn")
+    show_toc_btn.addEventListener("click", () => {
+        show_toc()
+    })
+
+    dialog.addEventListener("click", (event) => {
+        document.documentElement.style.overflow = "auto"
+        event.currentTarget.close()
+    })
+
+    document.querySelector(".post-content").appendChild(dialog)
+    document.querySelector(".post-content").appendChild(show_toc_btn)
+}
+
+function generate_toc() {
+    // https://stackoverflow.com/questions/187619/is-there-a-javascript-solution-to-generating-a-table-of-contents-for-a-page
+
+    let toc = ""
+    let level = 1
+
+    let container = document.querySelector(".post-content")
+    const regex = /<h([2-5])\s+id="([^"]+)">([^<]+)<\/h([2-5])>/gi
+    const matches = [...container.innerHTML.matchAll(regex)]
+    matches.forEach((match) => {
+        if (match.length != 5) return
+        const [_, openLevel, id, titleText, closeLevel] = match
+
+        if (openLevel !== closeLevel) {
+            return
+        }
+
+        if (openLevel > level) {
+            toc += new Array(openLevel - level + 1).join("<ol>")
+        } else if (openLevel < level) {
+            toc += new Array(level - openLevel + 1).join("</li></ol>")
+        } else {
+            toc += new Array(level + 1).join("</li>")
+        }
+
+        level = parseInt(openLevel)
+
+        if (!id) {
+            id = titleText.replace(/ /g, "_")
+        }
+        toc += '<li><a href="#' + id + '">' + titleText + "</a></li>"
+    })
+
+    if (level) {
+        toc += new Array(level + 1).join("</ol>")
+    }
+
+    return toc
+}
---
+++ main.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         freecodecamp-enhancer
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       renzhamin
+// @match        https://www.freecodecamp.org/news/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=freecodecamp.org
+// @resource     styles styles.css
+// @run-at       document-idle
+// ==/UserScript==
+
+const shortcut_toc_toggle = ";"
+
+window.onload = function () {
+    setTimeout(main, 2500)
+}
+
+function main() {
+    attach_copy_btn()
+    /* add_toc(shortcut_toc_toggle) */
+}
